97d310e8c058065a8fe6fc5762f8dc94194b6a0f
NatanelMunoz <natanael.munoz.s@gmail.com>
2023-06-09 07:35:02 -0500

    Inicio del proyecto del backend

---
+++ backendGestorAmbulancia/src/main/java/com/vv/prueba/BackendGestorAmbulanciaApplication.java
@@ -0,0 +1,13 @@
+package com.vv.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestorAmbulanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestorAmbulanciaApplication.class, args);
+	}
+
+}
---
+++ backendGestorAmbulancia/src/test/java/com/vv/prueba/BackendGestorAmbulanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vv.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestorAmbulanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
