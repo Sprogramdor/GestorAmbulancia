8eaff07f54d550f78d275154331795bc63f75567
MM4ur0 <ramos.maurom@ug.edu.ec>
2023-06-22 17:14:42 -0500

    LoginController

--- FrontendGestorAmbulancia/src/main/java/control/LoginController.java
+++ FrontendGestorAmbulancia/src/main/java/control/LoginController.java
@@ -10,18 +10,20 @@ import okhttp3.Response;
 import org.json.JSONObject;
 import view.FrmCPeticion;
 import view.FrmMenu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 /**
- *
  * @author MAURO FABRIZIO RAMOS MESIAS
-
+ *
  * Controlador para el inicio de sesión y recuperación de contraseña.
  */
 public class LoginController {
 
     public String url = "https://backendambulancia.onrender.com/vv/api/v1/ingresarAlSistema";
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
  
     /**
      * Inicia sesión en el sistema con las credenciales proporcionadas.
@@ -30,7 +32,7 @@ public class LoginController {
      * @param contrasena La contraseña.
      * @return true si el inicio de sesión fue exitoso, false si hubo un error.
      */
-    public boolean iniciarSesion(String usuario, String contrasena) {
+    public boolean iniciarSesion(final String usuario, final String contrasena) {
 
         if (usuario.equals("") && contrasena.equals("")) {
             JOptionPane.showMessageDialog(null, "Error: no ha ingresado las credenciales", "Error", JOptionPane.ERROR_MESSAGE);
@@ -44,28 +46,24 @@ public class LoginController {
             JOptionPane.showMessageDialog(null, "Error: no ha ingresado la contraseña.", "Error - No ha ingresado la contraseña", JOptionPane.ERROR_MESSAGE);
             return false;
         }
-        OkHttpClient cliente = new OkHttpClient();
+        final OkHttpClient cliente = new OkHttpClient();
 
-        MediaType mediaType = MediaType.parse("application/json");
-        String requestBody = "{\"nombreUsuario\": \"" + usuario + "\", \"contrasena\": \"" + contrasena + "\"}";
-        RequestBody resquestBody = RequestBody.create(mediaType, requestBody);
-
-        Request resquest = new Request.Builder()
+        final MediaType mediaType = MediaType.parse("application/json");
+        final String requestBody = "{\"nombreUsuario\": \"" + usuario + "\", \"contrasena\": \"" + contrasena + "\"}";
+        final RequestBody resquestBody = RequestBody.create(mediaType, requestBody);
+        final Request resquest = new Request.Builder()
                 .url(url)
                 .post(resquestBody)
                 .build();
 
         try (Response response = cliente.newCall(resquest).execute()) {
-            String responseBody = response.body().string();
-            System.out.println("Respuesta del servidor: " + responseBody);
-
+            final String responseBody = response.body().string();
             // Analizar el JSON de respuesta
-            JSONObject jsonObject = new JSONObject(responseBody);
-
+            final JSONObject jsonObject = new JSONObject(responseBody);
             // Obtener el valor del campo "tipo"
-            String tipo = jsonObject.getString("tipo");
+            final String tipo = jsonObject.getString("tipo");
              // Obtener el valor del campo "nombres
-            String nombre = jsonObject.getString("nombres");
+            final String nombre = jsonObject.getString("nombres");
             // Utilizar el valor de "tipo" según sea necesario
             if (tipo.equals("Gerente")) {
                 FrmMenu frmMenu = new FrmMenu();
@@ -76,11 +74,9 @@ public class LoginController {
                 frmcPeticion.show();
                 return true;
             }
-            System.out.println("Tipo: " + tipo);
         } catch (IOException e) {
-            System.out.println("Error al realizar la solicitud: " + e.getMessage());
+            LOGGER.error("Se produjo una excepción al obtener los clientes", e);
         }
-
         return false;
     }
     
@@ -90,23 +86,20 @@ public class LoginController {
      * @param usuario El nombre de usuario para el cual se desea recuperar la contraseña.
      * @return true si la recuperación de contraseña fue exitosa, false si hubo un error.
      */
-    public boolean recuperarContrasena(String usuario) {
+    public boolean recuperarContrasena(final String usuario) {
         if (usuario.equals("")) {
             JOptionPane.showMessageDialog(null, "Error: no ha ingresado el usuario", "Error", JOptionPane.ERROR_MESSAGE);
             return false;
         }
 
-        OkHttpClient cliente = new OkHttpClient();
-
+        final OkHttpClient cliente = new OkHttpClient();
         // Construir la URL con el parámetro usuario
-        String url = "https://backendambulancia.onrender.com/vv/api/v1/recuperarContrasena?correoElectronico=" + usuario;
-
-        Request resquest = new Request.Builder()
+        final String url = "https://backendambulancia.onrender.com/vv/api/v1/recuperarContrasena?correoElectronico=" + usuario;
+        final Request resquest = new Request.Builder()
                 .url(url)
                 .post(RequestBody.create(null, new byte[0]))
                 .build();
         try (Response response = cliente.newCall(resquest).execute()) {
-            String responseBody = response.body().string();
             JOptionPane.showMessageDialog(null, "Se ha enviado su contraseña a su correo, por favor espere unos minutos", "Éxito", JOptionPane.INFORMATION_MESSAGE);
             return true;
         } catch (IOException e) {
